e81a408f5d6e8998840bb0928de9b7628f783e56
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-09-16 23:40:24 +0100

    another commit

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,21 @@ import { useState, useEffect} from 'react';
 
 const Home = () => {
     const [quotes, setQuotes] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         fetch("http://localhost:8000/quote")
          .then(res => {
             return res.json();
         }).then(data => {
-            console.log(data);
+            setQuotes(data);
+            setIsLoading(true);
         })
     }, []);
 
     return (
         <div className="home">
-            <h2>Quotes</h2>
+            { quotes && <div>Home</div> }
         </div>
     );
 }

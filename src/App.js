ae8edf94700caf05380a0e4fde58653ced7ea4dc
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-12-02 21:45:35 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
         <div className="content">
           <Switch>
             <Route exact path="/">
-              <Home quotes={quotes} error={error} isLoading={isLoading} handleClick={handleClick} />
+              <Home quotes={quotes} error={error} isLoading={isLoading}/>
             </Route>
           </Switch>
         </div>

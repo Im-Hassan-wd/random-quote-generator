f87b626bfe856d106d49bd47880890c8cd5bba44
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-04 18:14:41 +0100

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { useState, useEffect} from 'react';
 import Home from "./Home";
 import data from "./data/db.json";
 import "./App.css";
 
 const randomNumber = Math.floor(Math.random() * 3);
-console.log(randomNumber);
 
 const App = () => {
-  const [quotes, setQuotes] = useState(null);
 
   return (
     <div className="app">

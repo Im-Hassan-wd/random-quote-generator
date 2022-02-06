559e5294e312bab864348dfb94c2ec99c15b3466
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-06 12:30:15 +0100

    adding author

--- src/RandomQuote.js
+++ src/RandomQuote.js
@@ -3,8 +3,11 @@ const RandomQuote = ({quotes}) => {
         <div className="random-quote">
             {quotes.map(quote =>(
                 <div className="quote-preview" key={quote.id}>
-                   <p>{ quote.body }</p>
-                   <div className="quote-author"> {quote.author} </div>
+                   <p className="quote-body">{ quote.body }</p>
+                   <div className="quote-description"> 
+                        <h3>{ quote.author }</h3>
+                        <p>{ quote.category}</p>
+                   </div>
                 </div>
             ))}
         </div>

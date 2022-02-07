a99673457543548bde2887d1d286b6e55bff6b9f
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-07 03:22:37 +0100

    whts happening

--- src/QuoteLists.js
+++ src/QuoteLists.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 const QuoteLists = () => {
     const { id } = useParams();
+    console.log(id);
 
     return (
         <div className="quote-list">

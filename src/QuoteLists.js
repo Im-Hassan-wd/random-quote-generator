8b17e1859d7419ab35f1cd5a2759a89192b5ae5d
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-10 22:17:21 +0100

    using the use Params

--- src/QuoteLists.js
+++ src/QuoteLists.js
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const QuoteLists = () => {
+    const {} = useParams();
+
     return (
         <div className="quote-list">
             <h2>Quote lists</h2>

e5e93ef4bbd595080eef81cf7cbaba419a2b5647
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-11 03:41:49 +0100

    new use effect

--- src/QuoteLists.js
+++ src/QuoteLists.js
@@ -3,6 +3,24 @@ import { useParams } from "react-router-dom";
 const QuoteLists = () => {
     const { author } = useParams();
 
+    useEffect(() => {
+      fetch("http://localhost:8000/quote")
+        .then(res => {
+          if(!res.ok) {
+            throw Error("Could not fecth the data");
+          }
+          return res.json();
+      }).then(data => {
+          setQuotes(data);
+          setIsLoading(false);
+          setError(null);
+      }).catch(err => {
+          setIsLoading(false);
+          setError(err.message);
+          console.log(err.message)
+      })
+  }, []);
+
     return (
         <div className="quote-list">
             <h2>Quote lists - { author } </h2>

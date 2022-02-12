ee39b4601a2df44bafc1124e3911c616fea6e735
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-12 18:02:49 +0100

    getting the clicked

--- src/RandomQuote.js
+++ src/RandomQuote.js
@@ -1,12 +1,16 @@
 const RandomQuote = ({quotes}) => {
     
+    const handleClick = (e) => {
+        console.log(e.target.children[0].textContent);
+    }
+
     return (
         <div className="random-quote">
             {quotes.map(quote =>(
                 <div className="quote-preview" key={quote.id}>
                    <p className="quote-body">{ quote.body }</p>
                    <div className="quote-description"> 
-                        <div onClick={(e) => console.log(e.target.children[0].textContent)}>
+                        <div onClick={handleClick}>
                             <h3>{ quote.author }</h3>
                             <p>{ quote.category}</p>
                         </div>

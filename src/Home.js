d1737d52d13f121c87665790d04f5505f29fa820
Hassan Warwick <82989580+Im-Hassan-wd@users.noreply.github.com>
2022-02-14 07:55:22 +0100

    Update home.js

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,14 @@
 import RandomQuote from './RandomQuote';
+import { useState} from "react";
 
 const Home = ({quotes, isLoading, error, handleClick}) => {
+    const [active, setActive] = useState("active");
     
     return (
         <div className="home">
             { isLoading && <div>Loading...</div>}
             { error && <div>{ error }</div>}
-            {quotes && <RandomQuote handleClick={handleClick} quotes={quotes} />}
+            {quotes && active === "active" ? <RandomQuote handleClick={handleClick} quotes={quotes} /> : null}
         </div>
     );
 }
